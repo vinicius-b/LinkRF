172ae33c93efbe88746de31ece75cb689beb61c7
mathias5r <mathiassilva4@gmail.com>
2016-09-03 11:07:24 -0300

    Changes in the framework class:
    Adding attributes to implement the FSM.
    Construction of the getters and setters methods.
    FSM implementation.

--- LinkRF/Framework.cpp
+++ LinkRF/Framework.cpp
@@ -7,8 +7,17 @@
 
 #include "Framework.h"
 
-Framework::~Framework() {
+using namespace std;
+
+Framework::Framework(Serial & s, int bytes_min, int bytes_max):serial(s) {
+	this->min_bytes = bytes_min;
+	this->max_bytes = bytes_max;
+	this->buffer[BUFSIZE];
+	this->n_bytes = 0;
+	this->currentState = waiting;
 }
+
+Framework::~Framework() {}
 
 /*
  * void Framming::send(char *buffer, int len);
@@ -22,36 +31,41 @@ Framework::~Framework() {
 void Framework::send(char *buffer, int len){
 
 	char frame[2*BUFSIZE+2];
-	
-	std::cout << "Framework started" << std::endl;
 
-	frame[0] = 0x7E;							// Initial delimitation of the frame using flag 0x7E (01111110)
-	int i,j;
-	for( i=0, j=1 ; i <= len ; i++,j++ ){
-		switch(buffer[i]){
+	cout << "Framework started" << endl;
+
+	if (len >= this->min_bytes && len <= this->max_bytes) {
+
+		frame[0] = 0x7E;							// Initial delimitation of the frame using flag 0x7E (01111110)
+		int i,j;
+		for( i=0, j=1 ; i <= len ; i++,j++ ){
+			switch(buffer[i]){
 			case(0x7E):							// If there is a 0x7E byte in the data, it is placed 0x7D5E in frame
-				frame[j] = 0x7D;
-				frame[j+1] = 0x5E;
-				j = j + 1;
-				std::cout << "INFO: 0x7E founded in position "<< i << " of the data." << std::endl;
-				break;
+														frame[j] = 0x7D;
+			frame[j+1] = 0x5E;
+			j = j + 1;
+			cout << "INFO: 0x7E founded in position "<< i << " of the data." << endl;
+			break;
 			case(0x7D):							// If there is a 0x7D byte in the data, it is placed 0x7D5D in frame
-				frame[j] = 0x7D;
-				frame[j+1] = 0x5D;
-				j = j + 1;
-				std::cout << "INFO: 0x7D founded in position "<< i << " of the data." << std::endl;
-				break;
+														frame[j] = 0x7D;
+			frame[j+1] = 0x5D;
+			j = j + 1;
+			cout << "INFO: 0x7D founded in position "<< i << " of the data." << endl;
+			break;
 			default:
 				frame[j] = buffer[i];			// Normal copy
+			}
 		}
-	}
-	frame[j-1] = 0x7E;							// End delimitation of the frame using flag 0x7E (01111110)
-	frame[j] = '\n';
+		frame[j-1] = 0x7E;							// End delimitation of the frame using flag 0x7E (01111110)
+		frame[j] = '\n';
 
-	int n = serial.write(frame, strlen(frame));
+		int n = serial.write(frame, strlen(frame));
 
-	if(! n > 0){
-		std::cout << "Error in writing a frame in the serial port" << n << std::endl;
+		if(! n > 0){
+			std::cout << "Error in writing a frame in the serial port" << n << std::endl;
+		}
+	} else {
+		cout << "ERROR: Frame exceeded the maximum/minimum size" << endl;
 	}
 }
 
@@ -65,54 +79,72 @@ void Framework::send(char *buffer, int len){
  * and put it in the buffer.
  *
  */
-void Framework::receive(char* buffer){
-	
-	std::cout << "Removal of framework started" << std::endl;
+int Framework::receive(char* buffer){
 
-	char frame[BUFSIZE];
+	cout << "Removal of framework started" << endl;
 
-	int n = serial.read(frame, BUFSIZE, false);
+	bool return_fsm;
+	char frame_byte;
 
-	if(! n > 0){
-		std::cout << "Error in reading a frame from serial port" << n << std::endl;
-	}else{
-		std::cout << "Frame received from serial: " << frame << std::endl;
+	for(int i=0;!return_fsm; i++){
+		this->serial.read(buffer, BUFSIZE, false);
+		frame_byte = buffer[i];
+		return_fsm = this->handle(frame_byte);
 	}
 
-	unsigned int start;							// Ignore any trash in the beginning of the frame
-	for(start = 0; start <= strlen(frame); start++){
-		if(frame[start] == 0x7E){
-			break;
+	cout << "Data received: " << buffer  << endl;
+
+	return this->n_bytes;
+}
+
+
+bool Framework::handle(char byte){
+
+
+	switch(this->currentState){
+
+	case waiting:
+		if(byte == 0x7E){
+			this->n_bytes = 0;
+			this->currentState = reception;
+		} else {
+			this->currentState = waiting;
 		}
-	}
+		break;
 
-	unsigned int i,j;
-	for(i = start+1, j = 0 ; i <= strlen(frame)-1 ; i++,j++ ){
-
-		if(frame[i] == 0x7E){					// Founded end delimitation
-			break;
-		}
-
-		if(frame[i] == 0x7D){					// Founded false flag
-			switch(frame[i+1]){
-				case(0x5E):
-					buffer[j] = 0x7E;
-					i = i+1;
-					std::cout << "0x7D5E found in position "<< i << " of the frame" << std::endl;
-					break;
-				case(0x5D):
-					buffer[j] = 0x7D;
-					i = i+1;
-					std::cout << "0x7D5D found in position "<< i << " of the frame" << std::endl;
-					break;
-				default:
-					buffer[j] = frame[i]; 		// Normal copy
-					break;
+	case reception:
+		if (this->n_bytes > this->max_bytes){
+			cout << "Overflow: " << this->n_bytes << " bytes" << endl;
+			this->currentState = waiting;
+		} else {
+			if(byte == 0x7E){
+				this->currentState = waiting;
+				return true; // frame finished
+			} if (byte == 0x7D) {
+				this->currentState = escape;
+			} else {
+				this->buffer[n_bytes] = byte;
+				this->n_bytes++;
+				this->currentState = reception;
 			}
-		}else{
-			buffer[j] = frame[i];				// Normal copy
 		}
+		break;
+	case escape:
+		switch(byte){
+		case(0x5E):
+    		this->buffer[n_bytes] = 0x7E;
+			this->currentState = reception;
+			break;
+		case(0x5D):
+			this->buffer[n_bytes] = 0x7D;
+			this->currentState = reception;
+			break;
+		default:
+			this->buffer[n_bytes] = byte;
+			this->currentState = reception;
+			break;
+		}
+		break;
 	}
-
-	std::cout << "Data received: " << buffer  << std::endl;
+	return false;
 }
--- LinkRF/Framework.h
+++ LinkRF/Framework.h
@@ -16,17 +16,43 @@
 class Framework {
 
 public:
-	Framework(Serial& s):serial(s){};
+	Framework(Serial& s, int bytes_min, int bytes_max);
 	virtual ~Framework();
 	void send(char *buffer, int len);
-	void receive(char* buffer);
+	int receive(char* buffer);
+
+	// getters
+	Serial * get_serial() { return serial; }
+	int get_max_bytes() { return max_bytes; }
+	int get_min_bytes() { return min_bytes; }
+	char * get_buffer() { return buffer; }
+
+	// setters
+	void set_buffer(char * buff) {buffer = buff; }
+	void set_serial(Serial & __serial) {serial = __serial; }
+	void set_min_bytes(int bytes_min) {min_bytes = bytes_min;}
+	void set_max_bytes(int bytes_max) {max_bytes = bytes_max;}
+
 private:
-	Serial& serial;
-//	int min_bytes, max_bytes;
-//	char *buffer;
-//	int n_bytes;
-//	int state;
-//	bool handle(char byte, int position);
+	Serial & serial;
+	int min_bytes, max_bytes; // max and min number of bytes allowed for each frame
+	char * buffer; // should be dimensioned instantiation
+
+	// bytes recebidos pela MEF até o momento
+	int n_bytes;
+
+	// estados para FSM
+	enum State {
+		waiting,reception,escape
+	};
+
+	State currentState;
+
+	// aqui se implementa a máquina de estados de recepção
+	// retorna true se reconheceu um quadro completo
+	bool handle(char byte);
+
+
 };
 
 #endif /* FRAMMING_H_ */
